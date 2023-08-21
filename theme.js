b342e9293f53f2197d75830cdbaad326d40eea23
Mryrs Estrella <124647809+0mryrs0@users.noreply.github.com>
2023-08-21 21:36:52 +0800

    updates

--- theme.js
+++ theme.js
@@ -1,6 +1,9 @@
 const theme = document.getElementById("theme");
 const body = document.querySelector(".body");
 
+
+var cssVar = document.querySelector(':root');
+
 function changeBackgroundColor(color) {
     document.body.style.backgroundColor = color;
 }
@@ -12,6 +15,9 @@ theme.addEventListener("click", function() {
     if (getComputedStyle(body).backgroundColor.toLowerCase() === "rgb(39, 40, 41)") {
         theme.innerHTML = `<i class="fa-solid fa-sun" style="color: #4e4e4e;></i>`
         changeBackgroundColor("#F5FEFD");
+        cssVar.style.setProperty('--background', '#ffffff');
+        cssVar.style.setProperty('---text-light', '#787878');
+        cssVar.style.setProperty('---text-dark', '#FFDB8');
     }
     else  {
         theme.innerHTML = "";
